d7f4dd81012700072faaf0fcc5b6bd7b5cb7f0de
Patrick Quan <patrickquan@Patricks-MBP.hitronhub.home>
2019-02-05 23:03:38 -0700

    updated gitignore

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://localhost:27017/patrick_quan_website",
-  passportSecret: "Spot is number one doggo and so is Luna",
-  nodemailerUser: "mySMTP.17@gmail.com",
-  nodemailerPass: "kuqVuz-tempab-4hurhy",
-  cloudinaryName: "dzpvgtcdp",
-  cloudinaryKey: "694265316154491",
-  cloudinarySecret: "EQfA1I-MAYQmfl2WzJHeAijgxo4"
-}
